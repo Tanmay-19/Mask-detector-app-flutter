e1bcf8a0a083b4fb39586fbbcde9c7cf24da41c7
Tanmay Chauhan <65083025+Tanmay-19@users.noreply.github.com>
2021-08-20 23:52:55 +0530

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'dart:io';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool loading = true;
+  late File _image;
+  late List _output;
+  final imagepicker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadmodel().then((value) {
+      setState(() {});
+    });
+  }
+
+  detectimage(File image) async {
+    var prediction = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+
+    setState(() {
+      _output = prediction!;
+      loading = false;
+    });
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+        model: 'assets/model.tflite', labels: 'assets/labels.txt');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // ignore: non_constant_identifier_names
+  pickimage_camera() async {
+    // ignore: deprecated_member_use
+    var image = await imagepicker.getImage(source: ImageSource.camera);
+    if (image == null) {
+      return null;
+    } else {
+      _image = File(image.path);
+    }
+    detectimage(_image);
+  }
+
+  // ignore: non_constant_identifier_names
+  pickimage_gallery() async {
+    // ignore: deprecated_member_use
+    var image = await imagepicker.getImage(source: ImageSource.gallery);
+    if (image == null) {
+      return null;
+    } else {
+      _image = File(image.path);
+    }
+    detectimage(_image);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mask Detector',
+          style: GoogleFonts.roboto(),
+        ),
+      ),
+      body: Container(
+        height: h,
+        width: w,
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              padding: EdgeInsets.all(10),
+              child: Image.asset('assets/mask-icon.png'),
+            ),
+            Container(
+                child: Text('Mask Detector',
+                    style: GoogleFonts.roboto(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ))),
+            SizedBox(height: 30),
+            Container(
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 118, right: 10, bottom: 30),
+                    // ignore: deprecated_member_use
+                    child: FloatingActionButton(
+                        child: Icon(Icons.camera),
+                        onPressed: () {
+                          pickimage_camera();
+                        }),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: EdgeInsets.only(left: 25, right: 10, bottom: 30),
+                    // ignore: deprecated_member_use
+                    child: FloatingActionButton(
+                        child: Icon(Icons.image),
+                        onPressed: () {
+                          pickimage_gallery();
+                        }),
+                  ),
+                ],
+              ),
+            ),
+            loading != true
+                ? Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 220,
+                          // width: double.infinity,
+                          padding: EdgeInsets.all(15),
+                          child: Image.file(_image),
+                        ),
+                        // ignore: unnecessary_null_comparison
+                        _output != null
+                            ? Text(
+                                (_output[0]['label']).toString().substring(2),
+                                style: GoogleFonts.roboto(fontSize: 18))
+                            : Text(''),
+                        // ignore: unnecessary_null_comparison
+                        _output != null
+                            ? Text(
+                                'Confidence: ' +
+                                    (_output[0]['confidence']).toString(),
+                                style: GoogleFonts.roboto(fontSize: 18))
+                            : Text('')
+                      ],
+                    ),
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
